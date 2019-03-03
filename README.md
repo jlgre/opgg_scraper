f5ad6ee15e1a76a9aa1fa83a6504d0e8891b0c7c
Luke Green <lukeg496@gmail.com>
2019-03-02 23:33:11 -0600

    added support for all lane tier lists

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a simple python scraper to pull down data from [opgg](https://op.gg)
 
-Currently supports top lane tier list
+Currently supports tier list for all lanes
--- scrape.py
+++ scrape.py
@@ -1,14 +1,65 @@
 import requests
 from bs4 import BeautifulSoup
 
-data = requests.get('https://na.op.gg/champion/statistics')
+data = requests.get("https://na.op.gg/champion/statistics")
 
-soup = BeautifulSoup(data.text, 'html.parser')
+soup = BeautifulSoup(data.text, "html.parser")
 
 ranks = soup.find("table", {"class" : "champion-index-table tabItems"})
-rtbody = ranks.find("tbody", {"class" : "tabItem champion-trend-tier-TOP"})
 
-for tr in rtbody.find_all("tr"):
+print("\nTOP TIER LIST\n")
+
+TOP = ranks.find("tbody", {"class" : "tabItem champion-trend-tier-TOP"})
+
+for tr in TOP.find_all("tr"):
+    raw = tr.find_all("td")
+    place = raw[0].text.strip()
+    name = raw[3].find("a").find("div").text.strip()
+    win_rate = raw[4].text.strip()
+    ban_rate = raw[5].text.strip()
+    print(place, name, win_rate, ban_rate)
+
+print("\nJUNGLE TIER LIST\n")
+
+JUNGLE = ranks.find("tbody", {"class" : "tabItem champion-trend-tier-JUNGLE"})
+
+for tr in JUNGLE.find_all("tr"):
+    raw = tr.find_all("td")
+    place = raw[0].text.strip()
+    name = raw[3].find("a").find("div").text.strip()
+    win_rate = raw[4].text.strip()
+    ban_rate = raw[5].text.strip()
+    print(place, name, win_rate, ban_rate)
+
+print("\nMID TIER LIST\n")
+
+MID = ranks.find("tbody", {"class" : "tabItem champion-trend-tier-MID"})
+
+for tr in MID.find_all("tr"):
+    raw = tr.find_all("td")
+    place = raw[0].text.strip()
+    name = raw[3].find("a").find("div").text.strip()
+    win_rate = raw[4].text.strip()
+    ban_rate = raw[5].text.strip()
+    print(place, name, win_rate, ban_rate)
+
+print("\nADC TIER LIST\n")
+
+ADC = ranks.find("tbody", {"class" : "tabItem champion-trend-tier-ADC"})
+
+for tr in ADC.find_all("tr"):
+    raw = tr.find_all("td")
+    place = raw[0].text.strip()
+    name = raw[3].find("a").find("div").text.strip()
+    win_rate = raw[4].text.strip()
+    ban_rate = raw[5].text.strip()
+    print(place, name, win_rate, ban_rate)
+
+print("\nSUPPORT TIER LIST\n")
+
+SUPPORT = ranks.find("tbody", {"class" : "tabItem champion-trend-tier-SUPPORT"})
+
+for tr in SUPPORT.find_all("tr"):
     raw = tr.find_all("td")
     place = raw[0].text.strip()
     name = raw[3].find("a").find("div").text.strip()
