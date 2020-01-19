3f4ce4fee0b7b4b744d752fcf9434acd44a330b3
Luke Green <lukeg496@gmail.com>
2020-01-18 23:46:51 -0600

    hopefully finish build

--- build.py
+++ build.py
@@ -6,34 +6,47 @@ def get(lane, name):
     """
     Gets build for a champion in a specific lane
     """
-    build = {
-        'starter_items1': [],
-        'starter_items2': [],
-        'build1': [],
-        'build2': [],
-        'build3': [],
-        'boot1': [],
-        'boot2': []
-    }
+    build = [
+        ('starter_items_1', []),
+        ('starter_items_2', []),
+        ('build_1', []),
+        ('build_2', []),
+        ('build_3', []),
+        ('build_4', []),
+        ('build_5', []),
+        ('boot_1', []),
+        ('boot_2', []),
+        ('boot_3', [])
+    ]
     data = requests.get("https://na.op.gg/champion/" +
                         name + "/statistics/" + lane)
     soup = BeautifulSoup(data.text, "html.parser")
     table = soup.find_all("table", {"class": "champion-overview__table"})
     tbody = table[1].find("tbody")
-    tr = tbody.find("tr", {"class": "champion-overview__row"})
-    td = tr.find("td", {"class": "champion-overview__data"})
-    ul = td.find("ul", {"class": "champion-stats__list"})
-    li = ul.find_all("li")
-    for i in li:
-        try:
-            mystr = i['title']
-            start = mystr.find('>') + 1
-            end = mystr.find('<', 1)
-            build['starter_items1'].append(mystr[start:end])
-        except KeyError:
-            pass
-    print(build)
+    tr = tbody.find_all("tr", {"class": "champion-overview__row"})
+    for i in range(0, 10):
+        td = tr[i].find("td", {"class": "champion-overview__data"})
+        ul = td.find("ul", {"class": "champion-stats__list"})
+        li = ul.find_all("li")
+        for j in li:
+            try:
+                mystr = j['title']
+                start = mystr.find('>') + 1
+                end = mystr.find('<', 1)
+                build[i][1].append(mystr[start:end])
+            except KeyError:
+                pass
+    return build
+
+
+def pretty_print(data):
+    for i in data:
+        build_items = ''
+        for j in i[1]:
+            build_items += j
+            build_items += ', '
+        print(i[0], ': ', build_items[:len(build_items) - 2])
 
 
 if __name__ == '__main__':
-    get('middle', 'annie')
+    pretty_print(get('mid', 'akali'))
--- scrape.py
+++ scrape.py
@@ -3,8 +3,11 @@ import sys
 
 import requests
 
+import build
 import skill_order
 import tier_list
+
+LANES = ["top", "TOP", "adc", "ADC", "mid", "MID", "jungle", "JUNGLE", "all", "ALL", "support", "SUPPORT"]
 
 
 def help_message():
@@ -17,7 +20,7 @@ def help_message():
 
 def main():
     try:
-        if sys.argv[1] == "-t" and sys.argv[2] in ["top", "TOP", "adc", "ADC", "mid", "MID", "jungle", "JUNGLE", "all", "ALL", "support", "SUPPORT"]:
+        if sys.argv[1] == "-t" and sys.argv[2] in LANES:
             lane = sys.argv[2]
             if lane == "all" or "ALL" or "All":
                 for indv in ["top", "jungle", "mid", "adc", "support"]:
@@ -29,6 +32,9 @@ def main():
         elif sys.argv[1] == "-so":
             skills = skill_order.get(sys.argv[3], sys.argv[2])
             skill_order.display(skills)
+        elif sys.argv[1] == '-b':
+            build_list = build.get(sys.argv[3], sys.argv[2])
+            build.pretty_print(build_list)
         else:
             help_message()
 
